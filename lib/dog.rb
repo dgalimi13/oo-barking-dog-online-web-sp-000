ef0d07d5604bcf75869e19b8215e41c5a82fab35
danny galimi <dannygalimi13@aim.com>
2020-09-02 22:26:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,14 @@
 class Dog 
   def name=(input)
-    this_dogs_name = input
+    @this_dogs_name = input
   end 
   
   def name 
-    this_dogs_name
+    @this_dogs_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
   
 end
